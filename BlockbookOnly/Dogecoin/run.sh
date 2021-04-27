7dbb34a68bd75576d9dbf56e86e51a360cbc4455
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2021-04-27 10:43:22 +0300

    Adding ports so network is accessible

--- BlockbookOnly/Dogecoin/run.sh
+++ BlockbookOnly/Dogecoin/run.sh
@@ -12,4 +12,4 @@ sed -i 's/\"rpc_pass\":.*/\"rpc_pass\": \"'${RPC_PASS}'\",/g' $CFG_FILE
 sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"http:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
 
 
-exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -workers=${WORKERS:-1}
+exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9138}
--- Ethereum/run.sh
+++ Ethereum/run.sh
@@ -8,7 +8,7 @@ MQ_PORT="${MQ_URL:-29000}"
 
 CFG_FILE=/home/blockbook/build/blockchaincfg.json
 
-exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} | \
+exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9136}| \
 geth console --ipcdisable --syncmode full --txlookuplimit 0 --cache 1024 --gcmode archive --port 38336 \
 --http --http.addr 127.0.0.1 --http.port=8136 --http.vhosts \'*\' --http.corsdomain \'*\' \
 --ws --ws.addr 127.0.0.1 --ws.port 8036 --ws.origins \'*\'
