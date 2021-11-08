d14895150fb49a685040b1d69dbf1d55126a8fe6
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2021-11-08 13:34:50 +0200

    Increasing the maximum number of open files

--- Ethereum/run.sh
+++ Ethereum/run.sh
@@ -8,6 +8,8 @@ MQ_PORT="${MQ_URL:-29000}"
 
 CFG_FILE=/home/blockbook/build/blockchaincfg.json
 
+sysctl -w fs.file-max=262144
+
 exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9136}| \
 geth console --ipcdisable --syncmode full --txlookuplimit 0 --cache 1024 --gcmode archive --port 38336 \
 --http --http.addr 127.0.0.1 --http.port=8136 --http.vhosts \'*\' --http.corsdomain \'*\' \
