c3e39a824f6e62776f22aee8c1b8af8634d4061f
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2022-01-31 16:55:52 +0200

    Adding BCH

---
+++ BlockbookOnly/BitcoinCash/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RPC_USER="${RPC_USER:-user}"
+RPC_PASS="${RPC_PASS:-pass}"
+RPC_HOST="${RPC_HOST:-localhost}"
+RPC_PORT="${RPC_PORT:-8355}"
+
+CFG_FILE=/home/blockbook/build/blockchaincfg.json
+
+sed -i 's/\"rpc_user\":.*/\"rpc_user\": \"'${RPC_USER}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_pass\":.*/\"rpc_pass\": \"'${RPC_PASS}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"http:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
+
+
+exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9131} -logtostderr
