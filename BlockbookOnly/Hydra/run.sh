65fa771ffb13d50848865c971862203c4ec82e4e
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2022-02-16 13:44:32 +0200

    Updating blockbook to be used from the local repo + adding Hydra config

---
+++ BlockbookOnly/Hydra/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RPC_USER="${RPC_USER:-user}"
+RPC_PASS="${RPC_PASS:-pass}"
+RPC_HOST="${RPC_HOST:-localhost}"
+RPC_PORT="${RPC_PORT:-3389}"
+
+CFG_FILE=/home/blockbook/build/blockchaincfg.json
+
+sed -i 's/\"rpc_user\":.*/\"rpc_user\": \"'${RPC_USER}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_pass\":.*/\"rpc_pass\": \"'${RPC_PASS}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"http:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
+
+
+
+exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9197} -logtostderr
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM gostartups/golang-rocksdb-zeromq
 
 WORKDIR /home
 # Build blockbook
-RUN git clone https://github.com/trezor/blockbook.git
+RUN git clone https://github.com/martin-key/blockbook.git
 
 WORKDIR /home/blockbook
 RUN go mod download
