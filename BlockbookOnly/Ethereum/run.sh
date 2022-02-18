7d45698055a22e17151fc3be02f23e35ada92ef3
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2022-02-18 21:44:09 +0200

    Revert "Adding extraflags to ETH"
    This reverts commit 437b77233d94c7520ce86a952e3d6d1eb7c01f99.

--- BlockbookOnly/Ethereum/run.sh
+++ BlockbookOnly/Ethereum/run.sh
@@ -3,11 +3,10 @@
 
 RPC_HOST="${RPC_HOST:-localhost}"
 RPC_PORT="${RPC_PORT:-8332}"
-EXTRAFLAGS="${EXTRAFLAGS}"
 
 
 CFG_FILE=/home/blockbook/build/blockchaincfg.json
 
 sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"ws:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
 
-exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9136} -logtostderr ${EXTRAFLAGS}
+exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9136} -logtostderr
