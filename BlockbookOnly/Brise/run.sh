a757984455aa8f47f03f77e7dc2ecfeed05ef844
Dean Donkov <nezpower@abv.bg>
2023-01-13 15:00:24 +0200

    Update brise node

--- BlockbookOnly/Brise/run.sh
+++ BlockbookOnly/Brise/run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-RPC_HOST="${RPC_HOST:-localhost}"
-RPC_PORT="${RPC_PORT:-8545}"
+RPC_USER="${RPC_USER:-}"
+RPC_PASS="${RPC_PASS:-}"
 WS_PORT="${WS_PORT:-8036}"
-ADDRESS="${ADDRESS:-0.0.0.0}"
-PORT="${PORT:-30303}"
-SYNCMODE="${full}"
-EXTRAFLAGS="${EXTRAFLAGS:-}"
-BOOTNODES="${BOOTNODES:-}"
+RPC_HOST="${RPC_HOST:-localhost}"
 
 CFG_FILE=/home/blockbook/build/blockchaincfg.json
 
+sed -i 's/\"rpc_user\":.*/\"rpc_user\": \"'${RPC_USER}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_pass\":.*/\"rpc_pass\": \"'${RPC_PASS}'\",/g' $CFG_FILE
 sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"ws:\/\/'${RPC_HOST}':'${WS_PORT}'\",/g' $CFG_FILE
 
 exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9122} -logtostderr
