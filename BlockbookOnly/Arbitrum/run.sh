d6384b1835d98a0378af8c6d723aafa503ab829b
Dean Donkov <nezpower@abv.bg>
2023-01-31 13:50:31 +0200

    Fix arbitrum blockbook port

--- BlockbookOnly/Arbitrum/run.sh
+++ BlockbookOnly/Arbitrum/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-RPC_HOST="${RPC_HOST:-localhost}"
-RPC_PORT="${RPC_PORT:-8547}"
+WS_RPC_HOST="${WS_RPC_HOST:-localhost}"
+WS_RPC_PORT="${WS_RPC_PORT:-8547}"
 
+HTTP_RPC_HOST="${HTTP_RPC_HOST:-localhost}"
+HTTP_RPC_PORT="${HTTP_RPC_PORT:-8546}"
 
 CFG_FILE=/home/blockbook/build/blockchaincfg.json
 
-sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"ws:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"ws:\/\/'${WS_RPC_HOST}':'${WS_RPC_PORT}'\",/g' $CFG_FILE
 
 #HTTP - If WS is down
-#sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"http:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
+#sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"http:\/\/'${HTTP_RPC_HOST}':'${HTTP_RPC_PORT}'\",/g' $CFG_FILE
 
-exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9136} -logtostderr
+exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9137} -logtostderr
