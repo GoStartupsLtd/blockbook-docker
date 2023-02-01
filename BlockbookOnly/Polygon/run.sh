e10cbca976048315ffdfe1608186ebf7af97f46e
Martin Kuvandzhiev <martinkuvandzhiev@gmail.com>
2023-02-01 18:52:30 +0200

    Update run.sh

--- BlockbookOnly/Polygon/run.sh
+++ BlockbookOnly/Polygon/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 RPC_HOST="${RPC_HOST:-localhost}"
-RPC_PORT="${RPC_PORT:-8547}"
+RPC_PORT="${RPC_PORT:-8099}"
 
 
 CFG_FILE=/home/blockbook/build/blockchaincfg.json
 
 sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"ws:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
 
-exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9144} -logtostderr
+exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9199} -logtostderr
