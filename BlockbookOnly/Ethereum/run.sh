3e25a8c4773f83222411f25e1a540388316af166
martinkuvandzhiev@gmail.com <martinkuvandzhiev@gmail.com>
2023-02-01 18:48:02 +0200

    Changes2023

--- BlockbookOnly/Ethereum/run.sh
+++ BlockbookOnly/Ethereum/run.sh
@@ -7,6 +7,6 @@ RPC_PORT="${RPC_PORT:-8332}"
 
 CFG_FILE=/home/blockbook/build/blockchaincfg.json
 
-sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"ws:\/\/'${RPC_HOST}':'${RPC_PORT}'\",/g' $CFG_FILE
+sed -i 's/\"rpc_url\":.*/\"rpc_url\": \"wss:\/\/tiniest-responsive-friday.quiknode.pro\/89df52c1f3cf3a83bc0ffe3de395cc71cf7b1733\",/g' $CFG_FILE
 
-exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -debug=true -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9136} -logtostderr
+exec ./blockbook -sync -blockchaincfg=/home/blockbook/build/blockchaincfg.json -workers=${WORKERS:-1} -public=:${BLOCKBOOK_PORT:-9136} -logtostderr -resyncindexperiod=60000 -resyncmempoolperiod=50213
--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM gostartups/golang-rocksdb-zeromq:andromeda
 
 WORKDIR /home
 # Build blockbook
-RUN git clone https://github.com/martin-key/blockbook.git
+RUN git clone https://github.com/changex-app/blockbook.git
 
 WORKDIR /home/blockbook
 RUN go mod download
 RUN go build -tags rocksdb_6_16
-
